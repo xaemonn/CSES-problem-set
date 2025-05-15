8e665faa61bcc4cbc9efecaefd298e4607d6ebe3
xaemonn <dishakwatra24@gmail.com>
2025-05-15 17:31:03 +0530

    problem 5

---
+++ Sum_of_Two_Values.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+void solve(){
+    ll n,sum; cin >> n>>sum;
+    vector<ll> v(n);
+    map<ll, ll> seen;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        
+    }
+
+    for (ll i = 0; i < n;i++){
+        ll need = sum - v[i];
+        if(seen.count(need)){
+            cout << seen[need] + 1 << " " << i + 1 << endl;
+            return;
+        }
+        seen[v[i]] = i;
+    }
+
+    cout << "IMPOSSIBLE" << endl;
+}
+
+int main(){
+    solve();
+}
